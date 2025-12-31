84761adf85fe24c02fa6b4fa3899508c1ed5cd93
Kithmal Wimalaratne <wimalaratnek@student.douglascollege.ca>
2025-12-30 18:10:35 -0800

    feat: Two pointers question setup

---
+++ dotnet/02.TwoPointers/LC_125_ValidPalindrome.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace TwoPointers;
+
+internal class LC_125_ValidPalindrome
+{
+
+    public static bool Question(string s)
+    {
+        
+        Func<bool>[] solutions = [
+            () => {
+                
+                return false;
+            } 
+        ]; 
+
+        return solutions[0].Invoke();
+    }
+
+    static void Main(string[] args)
+    {
+
+        bool test1 = Question("A man, a plan, a canal: Panama");
+        bool test2 = Question("race a car");
+        bool test3 = Question(" ");
+
+        Console.WriteLine($"{test1} {test2} {test3}");
+    }
+}
