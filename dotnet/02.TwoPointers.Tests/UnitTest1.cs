45f5cdd838d1a62044027be5208a6bbaf5d9af8a
Kithmal Wimalaratne <wimalaratnek@student.douglascollege.ca>
2025-12-30 17:53:49 -0800

    feat: Two Pointers related projects setup

---
+++ dotnet/02.TwoPointers.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.TwoPointers.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
