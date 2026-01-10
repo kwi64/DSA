a32446f22cc0f9a363237c75fc4e02612af0fe98
Kithmal Wimalaratne <k.n.wimalarathne@gmail.com>
2026-01-09 23:07:56 -0800

    feat: BinarySearch project and question setup

---
+++ dotnet/src/04_BinarySearch/LC_704_BinarySearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04_BinarySearch;
+
+class LC_704_BinarySearch
+{
+    public static int Search(int[] nums, int target)
+    {
+
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ dotnet/test/04_BinarySearch.Tests/LC_704_BinarySearchTest.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_BinarySearch.Tests;
+
+public class LC_704_BinarySearchTest
+{
+    [Test]
+    [TestCase(new int[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
+    [TestCase(new int[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
+    public void Search_TargetExistsOrNot_ReturnsExpectedResult(int[] nums, int target, int expected)
+    {
+        // Arrange
+
+        // Act
+        int actual = LC_704_BinarySearch.Search(nums, target);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
