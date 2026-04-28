3c41b31e705b1604d062cdd30ad342548d995ff9
Kithmal Wimalaratne <k.n.wimalarathne@gmail.com>
2026-04-27 20:26:53 -0700

    LC_242_ValidAnagram initial solution

--- dotnet/src/NC_01_ArraysAndHashing/LC_242_ValidAnagram/Program.cs
+++ dotnet/src/NC_01_ArraysAndHashing/LC_242_ValidAnagram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NC_01_ArraysAndHashing.LC_242_ValidAnagram
 {
@@ -6,8 +7,31 @@ namespace NC_01_ArraysAndHashing.LC_242_ValidAnagram
     {
         public static bool IsAnagram(string s, string t)
         {
-            if (s != t) return false;
-            return false;
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, int> letters = [];
+
+            foreach(char c in s)
+            {
+                if(!letters.TryAdd(c, 1))
+                {
+                    letters[c]++;
+                }
+            }
+
+            foreach(char c in t)
+            {
+                if(letters.TryGetValue(c, out int count) && count > 0)
+                {
+                    letters[c]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            return true;
         }
 
         static void Main(string[] args)
