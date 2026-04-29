7302dd0f9b874fb45b85191a422ca2a17774fdae
Kithmal Wimalaratne <k.n.wimalarathne@gmail.com>
2026-04-29 14:03:04 -0700

    LC_242_ValidAnagram tests

---
+++ dotnet/test/NC_01_ArraysAndHashing.Tests/LC_242_ValidAnagramTest.cs
@@ -0,0 +1,23 @@
+﻿using NC_01_ArraysAndHashing.LC_242_ValidAnagram;
+
+namespace NC_01_ArraysAndHashing.Tests
+{
+    [TestFixture]
+    internal class LC_242_ValidAnagramTest
+    {
+        [Test]
+        [TestCase("racecar", "carrace", true)]
+        [TestCase("jar", "jam", false)]
+        public void ValidAnagram_WithVariousInputs_ReturnsExpectedResults(string s, string t, bool expected)
+        {
+            // Arrange
+            // Arranged from the test case data
+
+            // Act
+            bool actual = Program.IsAnagram(s, t);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
