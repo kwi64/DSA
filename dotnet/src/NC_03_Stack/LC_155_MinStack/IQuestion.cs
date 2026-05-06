f17e46a35744ce760f0c3e9e1ffc730fdde16b8c
Kithmal Wimalaratne <k.n.wimalarathne@gmail.com>
2026-05-05 21:15:42 -0700

    LC_155 Question setup

---
+++ dotnet/src/NC_03_Stack/LC_155_MinStack/IQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NC_03_Stack.LC_155_MinStack
+{
+    internal interface IQuestion
+    {
+        abstract void Push(int val);
+        void Pop();
+        int Top();
+        int GetMin();
+    }
+}
---
+++ dotnet/src/NC_03_Stack/LC_155_MinStack/Problem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NC_03_Stack.LC_155_MinStack
+{
+    internal class Problem
+    {
+
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
